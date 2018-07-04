56fa3ab5e457b46c6171ecac6e58a1bec1f187ca
joyce <joycedelatorre87@gmail.com>
2018-07-04 15:22:27 -0500

    fix bugs

--- stocks.js
+++ stocks.js
@@ -1,14 +1,3 @@
-
-// dataSet = [
-// {"name": "Apple", "price": 191, "marketCap": 942},
-// {"name": "Google", "price": 1130, "marketCap": 791},
-// {"name": "Facebook", "price": 189, "marketCap": 556},
-// {"name": "Microsoft", "price": 101, "marketCap": 793},
-// {"name": "Oracle", "price": 48, "marketCap": 196},
-// {"name": "Tesla", "price": 317, "marketCap": 54},
-// {"name": "IBM", "price": 146, "marketCap": 133},
-// {"name": "Amazon", "price": 1683, "marketCap": 820}
-// ];
 var dataSet;
 
 $(document).ready(function(){
